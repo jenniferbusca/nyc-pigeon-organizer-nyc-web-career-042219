5cb5d926cf385b0b747b72ba2465dcc0570dec16
github username <github email address>
2019-05-09 09:30:58 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+def nyc_pigeon_organizer(pigeon_data)
+  hash = Hash.new
+  pigeon_data.each do |attribute, attribute_types|
+    attribute_types.each do |key, value|
+      value.each do |value_attribute|
+        if hash.include?(value_attribute)
+          hash[value_attribute][attribute].to_a << key.to_s
+        else
+          binding.pry
+          hash[value_attribute] = {attribute => [key.to_s]}
+        end
+      end
+    end
+  end
+  return hash
 end
